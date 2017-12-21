e1625777d1c607a8f9225cb69c66fb0c7fd20d41
george <george@georges-macbook-pro-2.local>
2017-12-21 16:21:02 +1000

    Initial Commit

---
+++ sigmoid velocity warp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sigmoid velocity warp
+//
+//  Created by george on 12/21/17.
+//  Copyright © 2017 george. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ sigmoid velocity warp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sigmoid velocity warp
+//
+//  Created by george on 12/21/17.
+//  Copyright © 2017 george. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
