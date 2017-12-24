45ffbdf06490725d9fe77159cb36a909e796807e
george <george@georges-macbook-pro-2.local>
2017-12-24 16:08:54 +1000

    v2a

--- sigmoid velocity warp/WarpView.swift
+++ sigmoid velocity warp/WarpView.swift
@@ -23,7 +23,7 @@ class WarpView: NSView {
         
         for i in 0...720 {
          
-            var timeShift = sineWave.amplitudeValues[i] * warpFactor
+            var timeShift = sineWave.adjustedAmplitudeValues[i] * warpFactor
            // Swift.print("time shift for phase \(i) = \(timeShift)")
             
             warpSinePath.line(to: NSPoint(x: CGFloat((sineWave.rawTimeValues[i] + timeShift)/8 * 720), y: CGFloat(CGFloat(sineWave.adjustedAmplitudeValues[i] * 200) + (self.bounds.height/2))))
